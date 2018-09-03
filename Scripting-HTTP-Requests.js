db528e7baad4d200752d92af425a0e41635d1836
Yang Yu <nicholas.yu24@gmail.com>
2018-09-03 18:40:38 +0000

    added buffer function

--- Scripting-HTTP-Requests.js
+++ Scripting-HTTP-Requests.js
@@ -29,4 +29,32 @@ function getAndPrintHTMLChunks () {
 
 }
 
-getAndPrintHTMLChunks();
+function getAndPrintHTML () {
+
+  var buffer = {};
+
+  var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step2.html'
+  };
+  // notice that https.get takes a callback with one parameter -
+  // response, which is a Stream that represents the HTTP response
+  https.get(requestOptions, function(response){
+    // set encoding of received data to UTF-8
+    response.setEncoding('utf8');
+
+    // the callback is invoked when a `data` chunk is received
+    response.on('data', function (data) {
+      buffer += data;
+    });
+    // the callback is invoked when all of the data has been received
+    // (the `end` of the stream)
+    response.on('end', function() {
+      console.log('Response stream complete.');
+      console.log('BUffer Received. :', buffer);
+    });
+  });
+
+}
+//getAndPrintHTMLChunks();
+getAndPrintHTML();
